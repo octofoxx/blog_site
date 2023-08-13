56b75ec43cf8a54099061f165b68805b26606dd3
octofoxx <devilmaycry87x@gmail.com>
2023-08-13 11:51:02 -0700

    made seed data

---
+++ seeds/seed.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models/allModels');
+
+const userData = require('./userSeeds.json');
+const blogData = require('./blogSeeds.json');
+const commentData = require('./commentSeeds.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    for (const blog of blogData) {
+      await Blog.create({
+        ...blog,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
+    }
+
+    const comments = await Comment.bulkCreate(commentData);
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();
