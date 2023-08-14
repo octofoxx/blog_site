aff5265da18e24212c631fa88c5c0022b85d73e1
octofoxx <devilmaycry87x@gmail.com>
2023-08-13 19:51:21 -0700

    changed editDelete to just handle delete for now

--- public/js/delete.js
+++ public/js/delete.js
@@ -13,22 +13,7 @@ const deleteBlog = async (event) => {
       alert(response.statusText);
     }
   };
-  
-  // Function created that allows for a user to edit blog by redirecting them to the /create/:id page
-  const editBlog = async (event) => {
-    event.preventDefault();
-
-    let blogId = event.target.getAttribute("data-id");
-  
-    document.location.assign(`/create/${blogId}`);
-  };
-  
-  const editButton = document.querySelectorAll("#editBtn");
-  
-  for (let i = 0; i < editButton.length; i++) {
-    editButton[i].addEventListener("click", editBlog);
-  }
-  
+   
   const deleteButton = document.querySelectorAll("#deleteBtn");
   
   for (let i = 0; i < deleteButton.length; i++) {
